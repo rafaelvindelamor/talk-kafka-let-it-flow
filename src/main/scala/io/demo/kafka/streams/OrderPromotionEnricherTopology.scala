676989dabf1eccea376dd41aa80cc992053b66b7
Rafael Vindel Amor <rafael.vindel@gmail.com>
2021-11-14 10:13:32 +0100

    simplify stream

--- src/main/scala/io/demo/kafka/streams/OrderPromotionEnricherTopology.scala
+++ src/main/scala/io/demo/kafka/streams/OrderPromotionEnricherTopology.scala
@@ -1,17 +1,18 @@
 package io.demo.kafka.streams
 
 import io.demo.kafka.streams.domain.{Order, OrderId, Promotion, PromotionId}
+import io.demo.kafka.streams.serdes._
 import org.apache.kafka.streams.Topology
-import org.apache.kafka.streams.kstream.{Named, Printed}
+import org.apache.kafka.streams.kstream.Named
 import org.apache.kafka.streams.scala.StreamsBuilder
 import org.apache.kafka.streams.scala.kstream.{Branched, Consumed, Produced}
 import org.apache.kafka.streams.scala.serialization.Serdes._
-import serdes._
 
 object OrderPromotionEnricherTopology {
 
   def apply(): Topology = {
 
+    // We start invoking StreamsBuilder
     val builder = new StreamsBuilder()
 
     // Stream of orders
@@ -19,30 +20,25 @@ object OrderPromotionEnricherTopology {
     // GlobalKTable of promotions
     val promotions = builder.globalTable("promotions")(Consumed.`with`[PromotionId, Promotion])
 
-    // Remove non-spanish orders as per business requirement
-    val filtered = orders.filter { (_, order) =>
-      order.country == "es"
-    }
-
     // Predicates for split
     val promotionPresent = (_: OrderId, order: Order) => order.promotionId.nonEmpty
-    val promotionNotPresent = (_: OrderId, order: Order) => order.promotionId.isEmpty
+    val promotionNotPresent = (orderId: OrderId, order: Order) => !promotionPresent(orderId, order)
 
     // Branching for promotion to apply present or not
-    val branches = filtered
+    val branches = orders
       .split(Named.as("promotion_"))
       .branch(promotionPresent, Branched.as("present"))
       .branch(promotionNotPresent, Branched.as("not_present"))
       .noDefaultBranch()
 
-    // If promotion is present apply it by joining with "promotions" GlobalKTable
+    // If promotion is present in the order, apply it by joining with promotions GlobalKTable
     val promotionApplied = branches("promotion_present")
       .join(promotions)(
         (_: OrderId, o: Order) => o.promotionId,
         (o: Order, p: Promotion) => o.copy(price = o.price * (1 - p.percentage))
       )
 
-    // Merge two branches back
+    // Merge promotions branches back
     val merged = branches("promotion_not_present").merge(promotionApplied)
 
     // Send all the orders to another topic
